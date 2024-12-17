a7e2c1b28bcc32162570a6d2bf9528423a97cde6
Firefoxmoo89 <firefoxmoo@gmail.com>
2024-12-16 23:04:58 -0700

    Major upheavel of the turn system

--- script/footscript.js
+++ script/footscript.js
@@ -2,7 +2,9 @@
 const myHand = document.querySelector("#myHand")
 const discard = document.querySelector("#discard");
 const deck = document.querySelector("#deck");
+const drawContainer = document.querySelector("#drawContainer");
 const draw = document.querySelector("#draw");
+const tracker = document.querySelector("#score");
 
 for (let slot of myHand.querySelectorAll("div.slot")) {
 	newCard = deckData.shift();
@@ -13,33 +15,61 @@ for (let slot of myHand.firstElementChild.querySelectorAll("div.slot")) {
 }
 document.querySelector("#discard").appendChild(deckData.shift().element);
 
+window.listenerList = [];
+function includeListener(element,event,daFunction) {
+	element.addEventListener(event,daFunction);
+	window.listenerList.push([element,event,daFunction]);
+}
+function purgeListeners() {
+	for ([element,event,daFunction] of window.listenerList) {
+		element.removeEventListener(event,daFunction);
+	} window.listenerList = [];
+}
+
 function drawAction() {
+	drawContainer.style.display = "none";
+	includeListener(discard,"click", fromDiscard);
+	includeListener(deck,"click", fromDeck);
 	function fromDiscard(event) { 
-		draw.appendChild(discard.firstChild);
-		discard.removeEventListener("click",fromDiscard);
-		deck.removeEventListener("click",fromDeck);
-		placeAction();
+		draw.appendChild(discard.firstElementChild);
+		purgeListeners();	placeAction();
 	}
 	function fromDeck(event) {
 		draw.appendChild(deckData.shift().element);
-		discard.removeEventListener("click",fromDiscard);
-		deck.removeEventListener("click",fromDeck);
-		placeAction();
+		purgeListeners();	placeAction(true);
 	}
-	discard.addEventListener("click", fromDiscard);
-	deck.addEventListener("click", fromDeck);
 }
-function placeAction() {
+function placeAction(fromDeck=false) {
+	drawContainer.style.display = "flex"; console.log("sup");
+	for (let slot of myHand.querySelectorAll("div.slot")) { includeListener(slot,"click",swapCards) }
+	if (fromDeck) { includeListener(discard,"click",trash) }
 	function swapCards(event) {
 		selected = event.target; while(selected.className != "slot") { selected = selected.parentElement }
 		if (selected.firstElementChild.dataset.flipped = "true") { selected.firstElementChild.dataset.flipped = "false" }
-		discard.firstElementChild.remove();
+		if (discard.firstElementChild != null) { discard.firstElementChild.remove() }
 		discard.appendChild(selected.firstElementChild);
 		selected.appendChild(draw.firstElementChild);
-		for (let slot of myHand.querySelectorAll("div.slot")) {	slot.removeEventListener("click",swapCards) }
-		drawAction();
+		purgeListeners();	drawAction(); calculateScore();
 	}
-	for (let slot of myHand.querySelectorAll("div.slot")) { slot.addEventListener("click",swapCards) }
+	function trash(event) {
+		if (discard.firstElementChild != null) { discard.firstElementChild.remove() }
+		discard.appendChild(draw.firstElementChild);
+		purgeListeners();	drawAction(); calculateScore();
+	}
 }
 
-drawAction();
+calculateScore(); drawAction();
+
+function calculateScore() {
+	tracker.innerText = 0;
+	[topRow,bottomRow] = myHand.querySelectorAll("div.row");
+	for (column=0;column<4;column++) {
+		topCard = topRow.children[column].firstElementChild; bottomCard = bottomRow.children[column].firstElementChild;
+		if (topCard.dataset.value != bottomCard.dataset.value && topCard.dataset.value != "P" && bottomCard.dataset.value != "P") {
+			tracker.innerText = Number(tracker.innerText)-Number(topCard.dataset.value)-Number(bottomCard.dataset.value);
+		} else if ((topCard.dataset.special == "Reinforcements" || bottomCard.dataset.special == "Reinforcements") && topCard.dataset.value == bottomCard.dataset.value) {
+			tracker.innerText = Number(tracker.innerText)+Number(topCard.dataset.value);
+		}
+	}
+	if (Number(tracker.innerText) > 0) { tracker.innerText = "+"+tracker.innerText }
+}
