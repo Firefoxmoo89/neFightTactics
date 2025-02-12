91a958742181c38b39a20d54535ed620f63ff394
Firefoxmoo89 <firefoxmoo@gmail.com>
2025-02-11 20:11:33 -0700

    Changed to exported module

--- script/headscript.js
+++ script/headscript.js
@@ -1,23 +1,24 @@
-class card {
+export class card {
 	constructor(value,special=false) {
 		this.value = value;
 		this.special = special;
 		this.element = document.createElement("div");
 		this.element.className = "card"; this.element.dataset.value = value; this.element.dataset.special = special;
 		this.element.innerHTML = `<h1>${value}</h1>`;
-		if (special) { this.element.innerHTML += `<h2>${special}</h2>` }
+		if (special && special!="false") { this.element.innerHTML += `<h2>${special}</h2>` }
 		this.element.innerHTML += "<div class=\"img\"></div>";
 	}
 }
 
-function shuffle(array) {
+export function shuffle(array, array2=false) {
 	for (let i=array.length-1;i>0;i--) {
 		let j = Math.floor(Math.random() * (i + 1));
-		[array[i],array[j]] = [array[j],array[i]];
+		if (!array2) { [array[i],array[j]] = [array[j],array[i]] }
+		else { [array[i],array[j]] = [array2[j],array2[i]] }
 	}
 }
 
-const deckData = [new card(0,"Assassin"),
+export const deckData = [new card(0,"Assassin"),
 	new card(1,"Extra Skill"),new card(2,"Extra Skill"),
 	new card(3,"Reinforcements"),new card(4,"Reinforcements"),new card(5,"Reinforcements"),new card(6,"Reinforcements"),
 	new card("P","Protection"),new card("","Protection")
@@ -28,3 +29,4 @@ for (let value=0;value<7;value++) {
 
 shuffle(deckData);
 
+export function getStyle(element,value,psuedoclass=null) {	return window.getComputedStyle(element,psuedoclass).getPropertyValue(value) }
