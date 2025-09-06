e0a0c3ae6219747432c31ec82d74a2aa671f8b3d
Firefoxmoo89 <firefoxmoo@gmail.com>
2025-09-05 18:43:37 -0600

    User is now added as a player, like the others. Initial card flipping has a different syntax and includes a random column flip

--- script/prep.js
+++ script/prep.js
@@ -1,4 +1,4 @@
-import {shuffle,getStyle,myHand,discard,discardData,deck,others,cardMoved,card, moveCards, slowDown, Player, cardInSlot} from "./define.js";
+import {shuffle,getStyle,myHand,discard,discardData,deck,others,cardMoved,card, moveCards, slowDown, Player, cardInSlot, getRandomInt} from "./define.js";
 const w = window;
 
 // Create cards and deck
@@ -11,8 +11,9 @@ for (let value=0;value<7;value++) {	for (let count=0;count<9;count++) {	w.deckDa
 shuffle(w.deckData);
 
 // Seat your friends
-new Player("Pizza Boi","#4545bb", "smileyface.svg", true)
-new Player("Donut Man","#99aa12", "smileyface.svg", true)
+new Player("User (Guest)","#aa1212","smileyface.svg",false,true);
+new Player("Pizza Boi","#4545bb", "smileyface.svg", true);
+new Player("Donut Man","#99aa12", "smileyface.svg", true);
 
 // Deal the Deck
 var allSlots = [discard].concat(Array.from(others.querySelectorAll("div.slot")),Array.from(myHand.querySelectorAll("div.slot")));
@@ -23,8 +24,17 @@ function flipStart(event) {
 	w.recentlyMoved++;
 	if (w.recentlyMoved == allSlots.length) { 
 		setTimeout(()=>{
-			for (let slot of myHand.querySelectorAll(".row")[1].querySelectorAll("div.slot")) {	slot.firstElementChild.classList.remove("flipped") }
-			discard.firstElementChild.classList.remove("flipped");
+			for (let player of Object.values(w.players)) {
+				if (player.user) { 
+					for (let index of [4,5,6,7]) { player.cardInIndex(index).flipUp()	}
+					player.cardInIndex(getRandomInt(3)).flipUp();
+				} else {
+					let randomInt = getRandomInt(3);
+					player.cardInIndex(randomInt).flipUp();
+					player.cardInIndex(randomInt+4).flipUp();
+				}
+			}
+			cardInSlot(discard).flipUp();
 			document.removeEventListener("cardMoved",flipStart);
 		},50);
 	}
