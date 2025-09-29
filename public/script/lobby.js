4a6e08e6074ef9750fa4e40e13f529aa8b907a51
Firefoxmoo89 <firefoxmoo@gmail.com>
2025-09-29 17:39:41 -0600

    Main lobby code made  - checks game data to load or reset a game  - fetches bot data to update bot into  - Changes name list based on selection "bot" or "player"  - regular updates need to be set up

--- public/script/lobby.js
+++ public/script/lobby.js
@@ -1,9 +1,36 @@
-let cardList = document.querySelectorAll("div.card");
+import * as App from "./app.js"
 
-for (let card of cardList) {
-	card.addEventListener("click", event => {
-		let daCard = event.target;
-		while (daCard.class != "card") { daCard = daCard.parentElement }
+await App.checkGameData();
+var gameData = localStorage.getItem("gameData");
+
+await App.fetchadids("/lobby", {botData: true}, data => {
+	localStorage.setItem("botData",JSON.stringify(data.botData));
+});
+
+let typeList = document.querySelectorAll("select.type");
+for (let typeSelect of typeList) {
+	typeSelect.addEventListener("change", event => {
+
+		let daValue = event.target.value.toLowerCase(); let array = []; console.log("daValue",daValue);
+		if (daValue == "bot") { array = Object.keys(JSON.parse(localStorage.botData)) }
+		else if (daValue == "player") { array = Object.keys(JSON.parse(localStorage.game.players)) }
 		
+		let daSlot = event.target; while (!daSlot.className.includes("slot")) { daSlot = daSlot.parentElement }
+		let daName = daSlot.querySelector("select.name");
+		let endText = "";
+
+		if (array.length == 0) { daName.innerText = "" }
+		else {
+			for (let character of array) {
+				endText += "<option>"+character+"</option>";
+			} daName.innerHTML = endText;
+		}
 	});
+}
+
+function regularUpdate(data) {
+	if (data) {
+		console.log();
+	}
+	App.fetchadids("/lobby", {update: true}, regularUpdate);
 }
