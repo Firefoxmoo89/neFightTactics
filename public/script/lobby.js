029d5b2a4bc66affcb24759d3a70e2b4ba6d542c
Firefoxmoo89 <firefoxmoo@gmail.com>
2026-01-26 09:45:45 -0700

    storing data and updating user

--- public/script/lobby.js
+++ public/script/lobby.js
@@ -1,10 +1,17 @@
 import * as App from "./app.js"
 
-await App.checkGameData();
-var gameData = localStorage.getItem("gameData");
+await App.fetchadids("/lobby", {register: true, profile: App.storage.profile}, data => {
+	App.storage.profile = data.profile;
+});
 
 await App.fetchadids("/lobby", {botData: true}, data => {
-	localStorage.setItem("botData",JSON.stringify(data.botData));
+	App.storage.botData = data.botData;
+});
+
+document.querySelector("#start").addEventListener("click", event => {
+	App.fetchadids("/lobby", { startGame: true }, data => {
+		if (data.success) { window.location.href = "/play" }
+	});
 });
 
 let typeList = document.querySelectorAll("select.type");
@@ -12,8 +19,10 @@ for (let typeSelect of typeList) {
 	typeSelect.addEventListener("change", event => {
 
 		let daValue = event.target.value.toLowerCase(); let array = []; console.log("daValue",daValue);
-		if (daValue == "bot") { array = Object.keys(JSON.parse(localStorage.botData)) }
-		else if (daValue == "player") { array = Object.keys(JSON.parse(localStorage.game.players)) }
+		if (daValue == "bot") { array = Object.keys(App.storage.botData) }
+		else if (daValue == "player") { 
+			for (let player of App.storage.gameData.players) { array.push(player.name) }
+		}
 		
 		let daSlot = event.target; while (!daSlot.className.includes("slot")) { daSlot = daSlot.parentElement }
 		let daName = daSlot.querySelector("select.name");
@@ -28,9 +37,13 @@ for (let typeSelect of typeList) {
 	});
 }
 
-function regularUpdate(data) {
-	if (data) {
-		console.log();
+function regularUpdate(data=undefined) {
+	if (data != undefined) {
+		if (data.players) { App.storage.players = data.players }
+		if (data.startGame) { window.location.href = "/play" }
 	}
-	App.fetchadids("/lobby", {update: true}, regularUpdate);
-}
+	App.fetchadids("/lobby", {
+		update: true, 
+		players: App.storage.players
+	}, regularUpdate);
+} regularUpdate();
